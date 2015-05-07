3ef15a589e02dc87ec136d44cf0e42d02e9bb351
Stefan Foulis <stefan@foulis.ch>
2015-05-07 19:49:46 +0200

    give redirects highest priority in middlewares

--- aldryn_config.py
+++ aldryn_config.py
@@ -3,5 +3,5 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
     def to_settings(self, data, settings):
-        settings['MIDDLEWARE_CLASSES'] += ['aldryn_redirects.middleware.RedirectFallbackMiddleware']
+        settings['MIDDLEWARE_CLASSES'].insert(0, 'aldryn_redirects.middleware.RedirectFallbackMiddleware')
         return settings
