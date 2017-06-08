0d0bcd3a8684e653bd71b87ef89d5aa1cda52f6a
Paulo <commonzenpython@gmail.com>
2017-06-08 09:43:33 -0400

    Changed the redirect lookup to ignore GET parameters

--- aldryn_redirects/middleware.py
+++ aldryn_redirects/middleware.py
@@ -1,26 +1,35 @@
 from django import http
 from django.conf import settings
+from django.db.models import Q
 
 from .models import Redirect
 
 
 class RedirectFallbackMiddleware(object):
+
     def process_request(self, request):
-        path = request.get_full_path()
+        path = request.path_info
+        path_with_queries = request.get_full_path()
+        queries = (
+            Q(old_path__iexact=path)
+            | Q(old_path__iexact=path_with_queries)
+        )
+
+        if settings.APPEND_SLASH and path.endswith('/'):
+            path_with_queries_no_slash = path[:-1] + path_with_queries[len(path):]
+            queries |= (
+                Q(old_path__iexact=path[:-1])
+                | Q(old_path__iexact=path_with_queries_no_slash)
+            )
+
         try:
-            r = Redirect.objects.get(site__id__exact=settings.SITE_ID, old_path=path)
+            r = Redirect.objects.filter(
+                queries,
+                site__id__exact=settings.SITE_ID
+            ).distinct().get()
         except Redirect.DoesNotExist:
-            r = None
-        if r is None and settings.APPEND_SLASH:
-            # Try removing the trailing slash.
-            try:
-                r = Redirect.objects.get(
-                    site__id__exact=settings.SITE_ID,
-                    old_path=path[:path.rfind('/')]+path[path.rfind('/')+1:]
-                )
-            except Redirect.DoesNotExist:
-                pass
-        if r is not None:
-            if r.new_path == '':
-                return http.HttpResponseGone()
-            return http.HttpResponsePermanentRedirect(r.new_path)
+            return
+
+        if r.new_path == '':
+            return http.HttpResponseGone()
+        return http.HttpResponsePermanentRedirect(r.new_path)
