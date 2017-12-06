ddc019940ee1f6e1606620986f975e3fcd5b878b
Filipe Waitman <filwaitman@gmail.com>
2017-12-06 13:33:49 -0200

    [WIP]

--- aldryn_redirects/forms.py
+++ aldryn_redirects/forms.py
@@ -4,6 +4,7 @@ from collections import defaultdict
 from tablib import Dataset
 
 from django import forms
+from django.core.exceptions import ValidationError
 from django.contrib.sites.models import Site
 from django.utils.translation import ugettext_lazy as _
 
@@ -58,6 +59,39 @@ def import_from_dataset(dataset):
     return new
 
 
+def validate_row(row, site_domain_id_mapping):
+    def _flatten_error(e):
+        result = []
+        for key, values in e.message_dict.items():
+            for value in values:
+                result.append('{}: {}'.format(key, value))
+
+        return '\n'.join(result)
+
+    if len(row) < 4:
+        raise ValidationError(_(
+            'malformed row. Row must contain site (required), '
+            'old_path (required), new_path (optional) and language_code (required).'
+        ))
+
+    domain, old_path, new_path, language = [x.strip() for x in row[:4]]
+
+    if domain not in site_domain_id_mapping.keys():
+        raise ValidationError(_('domain not found.'))
+
+    redirect = Redirect(site_id=site_domain_id_mapping[domain], old_path=old_path)
+    try:
+        redirect.full_clean(validate_unique=False)
+    except ValidationError as e:
+        raise ValidationError(_flatten_error(e))
+
+    redirect_translation = redirect.translations.model(master=redirect, language_code=language, new_path=new_path)
+    try:
+        redirect_translation.full_clean(validate_unique=False)
+    except ValidationError as e:
+        raise ValidationError(_flatten_error(e))
+
+
 class RedirectsImportForm(forms.Form):
     csv_file = forms.FileField(label=_('csv file'), required=True)
 
@@ -65,22 +99,13 @@ class RedirectsImportForm(forms.Form):
         csv_file = self.cleaned_data['csv_file']
         csv_file.seek(0)
         dataset = Dataset().load(csv_file.read().decode('utf-8'), format='csv')
-        allowed_domains = list(Site.objects.all().values_list('domain', flat=True))
+        site_domain_id_mapping = dict(Site.objects.values_list('domain', 'id'))
 
         for idx, row in enumerate(dataset, start=2):
-            if len(row) < 4:
-                raise forms.ValidationError(_('Row {}: malformed.'.format(idx)))
-
-            domain, old_path, new_path, language = [x.strip() for x in row[:4]]
-
-            if domain not in allowed_domains:
-                raise forms.ValidationError(_('Row {}: domain not found.'.format(idx)))
-
-            if not old_path:
-                raise forms.ValidationError(_('Row {}: old path is empty.'.format(idx)))
-
-            if not language:
-                raise forms.ValidationError(_('Row {}: language is empty.'.format(idx)))
+            try:
+                validate_row(row, site_domain_id_mapping)
+            except ValidationError as e:
+                raise forms.ValidationError('Line {}: {}'.format(idx, e))
 
         return csv_file
 
