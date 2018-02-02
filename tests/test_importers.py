55c2d80f1c33484155e1da28d951d4e05af95229
Filipe Waitman <filwaitman@gmail.com>
2018-02-02 17:02:12 -0200

    Add tests

--- tests/test_importers.py
+++ tests/test_importers.py
@@ -5,8 +5,8 @@ from django.contrib.sites.models import Site
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-from aldryn_redirects.importers import RedirectImporter
-from aldryn_redirects.models import Redirect
+from aldryn_redirects.importers import RedirectImporter, StaticRedirectImporter
+from aldryn_redirects.models import Redirect, StaticRedirect
 
 
 class RedirectImporterGetExistingRedirectsTestCase(TestCase):
@@ -92,3 +92,83 @@ class RedirectImporterValidateRowTestCase(TestCase):
     def test_language_code_invalid(self):
         self.row[3] = 'de'  # Not declared as a language in our tests/settings.py
         self.assertRaises(ValidationError, self.validate_row, self.row)
+
+
+class StaticRedirectImporterValidateRowTestCase(TestCase):
+    def setUp(self):
+        super(StaticRedirectImporterValidateRowTestCase, self).setUp()
+        self.row = [Site.objects.first().domain, '/origin/path', '/destination/path']
+        self.validate_row = StaticRedirectImporter().validate_row
+
+    def test_success(self):
+        self.validate_row(self.row)  # Nothing raised
+
+    def test_malformed_row(self):
+        del self.row[2:]
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_domain_missing(self):
+        self.row[0] = ''
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_domain_invalid(self):
+        self.row[0] = 'unknown.domain.com'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_inbound_route_missing(self):
+        self.row[1] = ''
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_inbound_route_too_large(self):
+        self.row[1] = '/path' * 100
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_inbound_route_sanity(self):
+        # NOTE: Use of query params in inbound_route is disallowed in models, but importer deals with that
+        self.row[1] = '/origin?key1=value1'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[1] = 'http://example.com/origin'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+        self.row[1] = 'origin'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+        self.row[1] = '/origin/'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+        self.row[1] = '/<script>hello</script>'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_outbound_route_missing(self):
+        self.row[2] = ''
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_outbound_route_too_large(self):
+        self.row[2] = '/path' * 100
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_outbound_route_sanity(self):
+        self.row[2] = 'http://example.com/destination'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[2] = 'https://example.com/destination'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[2] = '/destination'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[2] = '/destination/'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[2] = '/destination/?xxx=yyy'
+        self.validate_row(self.row)  # Nothing raised
+
+        self.row[2] = 'www.google.com/destination/?xxx=yyy'
+        self.assertRaises(ValidationError, self.validate_row, self.row)
+
+    def test_new_duplicated_row_is_ok(self):
+        ''' Dupes will be handled on StaticImporter.import_from_dataset as if they are updates. '''
+        redirect = StaticRedirect.objects.create(inbound_route='/origin/path', outbound_route='/destination/path')
+        redirect.sites.add(Site.objects.first())
+        self.validate_row(self.row)  # Nothing raised
